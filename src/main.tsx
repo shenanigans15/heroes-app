e6b78631c0a84e5ce0843792411c3015cf2d8129
Adrian Fayos <adrianfayos1234@gmail.com>
2026-03-06 19:49:51 -0300

    implement firs steps of routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import { HeroesApp } from './HeroesApp'
+
 import './index.css'
-import { Button } from './components/ui/button'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Hola Mundo</h1>
-    <Button>Hola de nuevo</Button>
+    <HeroesApp />
   </StrictMode>,
 )
