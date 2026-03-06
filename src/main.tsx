1b45eb598993ee9351f8eceb892e6c5d10cddab2
Adrian Fayos <adrianfayos1234@gmail.com>
2026-03-05 21:28:42 -0300

    create heroes app structure

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { Button } from './components/ui/button'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <h1>Hola Mundo</h1>
+    <Button>Hola de nuevo</Button>
+  </StrictMode>,
+)
