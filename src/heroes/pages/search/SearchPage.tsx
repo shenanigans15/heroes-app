324d87e07167ebfdcde8fe9d86e0ffe9be10880f
Adrian Fayos <adrianfayos1234@gmail.com>
2026-03-17 12:49:05 -0300

    Create components to split code

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,3 +1,22 @@
+import { CustomJumbotron } from '@/components/custom/CustomJumbotron'
+import { HeroStats } from '@/heroes/components/HeroStats'
+import { SearchControls } from './ui/SearchControls'
+
 export const SearchPage = () => {
-  return <div>SearchPage</div>
+  return (
+    <>
+      <CustomJumbotron
+        title="Búsqueda de Heroes de SuperHéroes"
+        description="Descubre, explora y administra super héroes y villanos"
+      />
+
+      {/* Stats Dashboard */}
+      <HeroStats />
+
+      {/* Filter and search */}
+      <SearchControls />
+    </>
+  )
 }
+
+export default SearchPage
--- src/router/app.routes.tsx
+++ src/router/app.routes.tsx
@@ -1,24 +1,43 @@
+import { lazy } from 'react'
+import { createBrowserRouter } from 'react-router'
+
+import { AdminLayout } from '@/admin/layouts/AdminLayout'
 import { AdminPage } from '@/admin/pages/AdminPage'
+import { HeroesLayout } from '@/heroes/layouts/HeroesLayout'
 import { HeroPage } from '@/heroes/pages/hero/HeroPage'
 import { HomePage } from '@/heroes/pages/home/HomePage'
-import { SearchPage } from '@/heroes/pages/search/SearchPage'
-import { createBrowserRouter } from 'react-router'
+// import { SearchPage } from '@/heroes/pages/search/SearchPage'
+
+const SearchPage = lazy(() => import('@/heroes/pages/search/SearchPage'))
 
 export const appRouter = createBrowserRouter([
   {
     path: '/',
-    element: <HomePage />,
+    element: <HeroesLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'heroes/1',
+        element: <HeroPage />,
+      },
+      {
+        path: 'search',
+        element: <SearchPage />,
+      },
+    ],
   },
-  {
-    path: '/heroes/1',
-    element: <HeroPage />,
-  },
-  {
-    path: '/search',
-    element: <SearchPage />,
-  },
+
   {
     path: '/admin',
-    element: <AdminPage />,
+    element: <AdminLayout />,
+    children: [
+      {
+        index: true,
+        element: <AdminPage />,
+      },
+    ],
   },
 ])
