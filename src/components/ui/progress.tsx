d4fbb67333d43456cb52c6bb7b2d09f7143f4b4d
Adrian Fayos <adrianfayos1234@gmail.com>
2026-03-17 20:22:48 -0300

    Create more components

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -1,25 +1,28 @@
-import * as React from "react"
-import { Progress as ProgressPrimitive } from "radix-ui"
+import * as React from 'react'
+import { Progress as ProgressPrimitive } from 'radix-ui'
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils'
 
 function Progress({
   className,
   value,
+  activeColor = 'bg-primary',
   ...props
-}: React.ComponentProps<typeof ProgressPrimitive.Root>) {
+}: React.ComponentProps<typeof ProgressPrimitive.Root> & {
+  activeColor?: string
+}) {
   return (
     <ProgressPrimitive.Root
       data-slot="progress"
       className={cn(
-        "relative h-2 w-full overflow-hidden rounded-full bg-primary/20",
-        className
+        'bg-primary/20 relative h-2 w-full overflow-hidden rounded-full bg-primary/20',
+        className,
       )}
       {...props}
     >
       <ProgressPrimitive.Indicator
         data-slot="progress-indicator"
-        className="h-full w-full flex-1 bg-primary transition-all"
+        className={cn(activeColor, 'h-full w-full flex-1 transition-all')}
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
       />
     </ProgressPrimitive.Root>
