9fa61aafe203a2be4b685cfbadbd9d89c1a1e431
Adrian Fayos <adrianfayos1234@gmail.com>
2026-03-23 20:48:33 -0300

    Implement search and filters functionality

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -2,8 +2,23 @@ import { CustomJumbotron } from '@/components/custom/CustomJumbotron'
 import { HeroStats } from '@/heroes/components/HeroStats'
 import { SearchControls } from './ui/SearchControls'
 import { CustomBreadcrumbs } from '@/components/custom/CustomBreadcrumbs'
+import { useQuery } from '@tanstack/react-query'
+import { HeroGrid } from '@/heroes/components/HeroGrid'
+import { useSearchParams } from 'react-router'
+import { searchHeroesAction } from '@/heroes/actions/search-heroes.action'
 
 export const SearchPage = () => {
+  const [searchParams] = useSearchParams()
+
+  const name = searchParams.get('name') ?? undefined
+  const strength = searchParams.get('strength') ?? undefined
+
+  const { data: heroes = [] } = useQuery({
+    queryKey: ['search', { name, strength }],
+    queryFn: () => searchHeroesAction({ name, strength }),
+    staleTime: 1000 * 60 * 5,
+  })
+
   return (
     <>
       <CustomJumbotron
@@ -21,6 +36,9 @@ export const SearchPage = () => {
 
       {/* Filter and search */}
       <SearchControls />
+
+      {/* Grid */}
+      <HeroGrid heroes={heroes} />
     </>
   )
 }
