319475f671c511aeb7715fb5870e9c6e570c8123
Adrian Fayos <adrianfayos1234@gmail.com>
2026-03-26 20:11:42 -0300

    Updates for deploy

--- src/router/app.routes.tsx
+++ src/router/app.routes.tsx
@@ -1,5 +1,5 @@
 import { lazy } from 'react'
-import { createBrowserRouter, Navigate } from 'react-router'
+import { createHashRouter, Navigate } from 'react-router'
 
 import { AdminLayout } from '@/admin/layouts/AdminLayout'
 import { AdminPage } from '@/admin/pages/AdminPage'
@@ -10,7 +10,8 @@ import { HomePage } from '@/heroes/pages/home/HomePage'
 
 const SearchPage = lazy(() => import('@/heroes/pages/search/SearchPage'))
 
-export const appRouter = createBrowserRouter([
+// export const appRouter = createBrowserRouter([
+export const appRouter = createHashRouter([
   {
     path: '/',
     element: <HeroesLayout />,
